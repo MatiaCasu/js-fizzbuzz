033a8f5641404ff1a7cd072c7e07afb7551b656d
Matia <matia.casu@gmail.com>
2021-03-02 15:53:46 +0100

    Contatore e multipli 3/5

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Contatore 1/100
+for (var i = 1; i <= 50; i++) {
+
+  if (i % 3 == 1 && i % 5 == 1){
+    console.log("FizzBuzz");
+  }
+  else if( i % 3 == 1 ){
+  console.log("Fizz");
+  }
+  else if ( i % 5 == 1 ){
+    console.log("Buzz");
+  }
+  console.log(i);
+}
+// /Programma che scriva i numeri 1/100
